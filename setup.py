70e2446cf851576c53ee5f20a615bca967580761
Wayne Manselle <wayne.manselle@belco.tech>
2019-09-17 09:24:01 -0700

    Initial License Additions
    Adding license information to the cached_property decorator so it can be included with the mffpy OSS release

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from distutils.core import setup
 setup(
     name='cached_property',
     version='1.0.0',
+    author='Justus Schwabedal',
+    author_email='jschwabedal@belco.tech',
+    maintainer='Justus Schwabedal',
+    maintainer_email='jschwabedal@belco.tech',
     packages=['cached_property'],
     long_description=open('README.md').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License :: Version 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
