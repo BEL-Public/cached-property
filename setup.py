86e057d674baade0d6e6da8341b7ef1e3a835d60
Wayne Manselle <wayne.manselle@belco.tech>
2019-09-25 12:43:25 -0700

    Bringing setup.py fully into alignment for upload to pypi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Apache License :: Version 2.0",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
